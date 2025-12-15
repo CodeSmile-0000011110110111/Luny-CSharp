0eb0f0872c03d39e36cfbda3960ad84b8979df62
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-15 20:47:08 +0100

    add throw

--- Throw.cs
+++ Throw.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 
 namespace Luny
 {
-	public sealed class Throw
-	{
-		public static void LifecycleAdapterSingletonDuplicationException(String adapterTypeName, String existingObjectName,
-			Int64 existingInstanceId, String duplicateObjectName, Int64 duplicateInstanceId) => throw new InvalidOperationException(
-			$"Duplicate {adapterTypeName} singleton detected! " +
-			$"Existing: Name='{existingObjectName}' InstanceID={existingInstanceId}, " +
-			$"Duplicate: Name='{duplicateObjectName}' InstanceID={duplicateInstanceId}");
+    public sealed class Throw
+    {
+        public static void LifecycleAdapterSingletonDuplicationException(String adapterTypeName, String existingObjectName,
+            Int64 existingInstanceId, String duplicateObjectName, Int64 duplicateInstanceId) => throw new InvalidOperationException(
+            $"Duplicate {adapterTypeName} singleton detected! " +
+            $"Existing: Name='{existingObjectName}' InstanceID={existingInstanceId}, " +
+            $"Duplicate: Name='{duplicateObjectName}' InstanceID={duplicateInstanceId}");
 
-		public static void LifecycleAdapterPrematurelyRemovedException(String godotLifecycleAdapterName) =>
-			throw new InvalidOperationException(
-				$"{godotLifecycleAdapterName} unexpectedly removed from SceneTree! It must remain in scene at all times.");
-	}
+        public static void LifecycleAdapterPrematurelyRemovedException(String godotLifecycleAdapterName) =>
+            throw new InvalidOperationException(
+                $"{godotLifecycleAdapterName} unexpectedly removed from SceneTree! It must remain in scene at all times.");
+    }
 }
