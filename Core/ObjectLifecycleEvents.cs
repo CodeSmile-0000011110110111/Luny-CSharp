78eed6dee6ec16362b6f38ec475d2847314cb764
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-30 13:54:16 +0100

    remove object lifecycle [Flags]

--- Core/ObjectLifecycleEvents.cs
+++ Core/ObjectLifecycleEvents.cs
@@ -5,32 +5,32 @@ namespace Luny
 	/// <summary>
 	/// LunyObject lifecycle events.
 	/// </summary>
-	[Flags] public enum ObjectLifecycleEvents
+	public enum ObjectLifecycleEvents
 	{
 		/// <summary>
 		/// Runs immediately after object was instantiated.
 		/// Unity: Awake | Godot: _init
 		/// </summary>
-		OnCreate = 1 << 0,
+		OnCreate = 0,
 		/// <summary>
 		/// Runs at the end of the frame in which object was requested to be destroyed.
 		/// Object is disabled and hidden between request to destroy and actual destruction.
 		/// Unity: OnDestroy | Godot: N/A
 		/// </summary>
-		OnDestroy = 1 << 1,
+		OnDestroy = 1,
 
 		/// <summary>
 		/// Runs immediately after object was instantiated (right after OnCreate) or when a disabled object gets enabled.
 		/// The object is technically already enabled during this event.
 		/// Unity: OnEnable | Godot: _enter_tree
 		/// </summary>
-		OnEnable = 1 << 2,
+		OnEnable = 2,
 		/// <summary>
 		/// Runs immediately when object is requested to be destroyed, or when a enabled object gets disabled.
 		/// The object is technically already disabled during this event.
 		/// Unity: OnDisable | Godot: _exit_tree
 		/// </summary>
-		OnDisable = 1 << 3,
+		OnDisable = 3,
 
 		/// <summary>
 		/// Runs in sync with engine's fixed update / physics processing at a fixed rate.
@@ -39,22 +39,22 @@ namespace Luny
 		/// CAUTION: Unsuitable for Input Event handling! Input events may be missed, especially first-frame triggers.
 		/// Unity: FixedUpdate | Godot: _physics_process
 		/// </summary>
-		OnFixedStep = 1 << 4,
+		OnFixedStep = 4,
 		/// <summary>
 		/// Runs in sync with engine's update.
 		/// Unity: Update | Godot: _process
 		/// </summary>
-		OnUpdate = 1 << 5,
+		OnUpdate = 5,
 		/// <summary>
 		/// Runs in sync with engine's update, directly after OnUpdate.
 		/// Unity: LateUpdate | Godot: N/A
 		/// </summary>
-		OnLateUpdate = 1 << 6,
+		OnLateUpdate = 6,
 
 		/// <summary>
 		/// Runs once per lifetime, just before the first call to either OnFixedStep (if it runs in this frame) or OnUpdate.
 		/// Unity: Start | Godot: _ready
 		/// </summary>
-		OnReady = 1 << 7,
+		OnReady = 7,
 	}
 }
