cc53ddc28f4dc196c2244451c74dcf5c86b15a41
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-03 14:28:08 +0100

    move engine adapter instance check to interface

--- Core/IEngineAdapter.cs
+++ Core/IEngineAdapter.cs
@@ -1,7 +1,18 @@
-﻿namespace Luny
+﻿using Luny.Exceptions;
+using System;
+
+namespace Luny
 {
 	/// <summary>
 	/// Marker interface for the native engine adapter.
 	/// </summary>
-	public interface IEngineAdapter {}
+	public interface IEngineAdapter
+	{
+		static IEngineAdapter EnsureSingleInstance(IEngineAdapter existingInstance, Object current)
+		{
+			if (existingInstance != null)
+				LunyThrow.EngineAdapterSingletonDuplicationException(typeof(IEngineAdapter), existingInstance, current);
+			return (IEngineAdapter)current;
+		}
+	}
 }
--- Exceptions/LunyThrow.cs
+++ Exceptions/LunyThrow.cs
@@ -1,29 +1,38 @@
 using System;
+using System.Diagnostics;
 
 namespace Luny.Exceptions
 {
 	internal sealed class LunyThrow
 	{
-		// [StackTraceHidden] not supported by Unity 6
-		public static void EngineAdapterSingletonDuplicationException(String adapterTypeName, String existingObjectName,
-			Int64 existingInstanceId, String duplicateObjectName, Int64 duplicateInstanceId) => throw new LunyLifecycleException(
-			$"Duplicate {adapterTypeName} singleton detected! " +
-			$"Existing: Name='{existingObjectName}' InstanceID={existingInstanceId}, " +
-			$"Duplicate: Name='{duplicateObjectName}' InstanceID={duplicateInstanceId}");
+#if GODOT
+		[StackTraceHidden] // not supported by Unity 6
+#endif
+		public static void EngineAdapterSingletonDuplicationException(Type instanceType, Object existingInstance, Object duplicateInstance) =>
+			throw new LunyLifecycleException($"Duplicate {instanceType?.Name} singleton detected! " +
+			                                 $"Existing: {existingInstance}, Duplicate: {duplicateInstance}");
 
-		// [StackTraceHidden] not supported by Unity 6
+#if GODOT
+		[StackTraceHidden] // not supported by Unity 6
+#endif
 		public static void SingletonDuplicationException(String typeName) =>
 			throw new LunyLifecycleException($"Duplicate {typeName} singleton detected!");
 
-		// [StackTraceHidden] not supported by Unity 6
+#if GODOT
+		[StackTraceHidden] // not supported by Unity 6
+#endif
 		public static void EngineAdapterPrematurelyRemovedException(String adapterName) => throw new LunyLifecycleException(
 			$"{adapterName} unexpectedly removed from SceneTree! It must remain in scene at all times.");
 
-		// [StackTraceHidden] not supported by Unity 6
+#if GODOT
+		[StackTraceHidden] // not supported by Unity 6
+#endif
 		public static void ServiceMustImplementSpecificInterfaceException(String serviceName) => throw new LunyServiceException(
 			$"Service {serviceName} must implement a specific interface derived from {nameof(IEngineService)}, not {nameof(IEngineService)} directly.");
 
-		// [StackTraceHidden] not supported by Unity 6
+#if GODOT
+		[StackTraceHidden] // not supported by Unity 6
+#endif
 		public static void ServiceImplementsMultipleInterfacesException(String serviceName, String interfacesFound) =>
 			throw new LunyServiceException(
 				$"Service {serviceName} implements multiple {nameof(IEngineService)}-derived interfaces: {interfacesFound}. Only one is allowed per type.");
