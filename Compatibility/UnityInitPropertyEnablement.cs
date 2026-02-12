4dd60fb6776c24aca7d9b0ddbf724d5d72ad386e
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-12 11:30:53 +0100

    fix compile errors, simplify script namespaces and class names

---
+++ Compatibility/UnityInitPropertyEnablement.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+// Only compile this if we are below .NET 5.0
+// Unity 6.3+ uses either .NET Standard 2.1 or .NET Framework 4.8 so it will be active in pre-CoreCLR versions of Unity
+#if !NET_5_0_OR_GREATER
+
+// ReSharper disable CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+	/// <summary>
+	/// Required for the 'init' property keyword to compile in Unity.
+	/// Without this class, the following code snippet would fail to compile in Unity 6:
+	/// `public String Name { get; init; }`
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	internal static class IsExternalInit {}
+}
+
+#endif
