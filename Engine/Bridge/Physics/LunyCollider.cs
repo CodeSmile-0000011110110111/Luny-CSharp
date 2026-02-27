7df979fdf039d87a6067f02584d11b93dbdfe814
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-27 12:34:33 +0100

    refactor collision/collider and builder

--- Engine/Bridge/Physics/LunyCollider.cs
+++ Engine/Bridge/Physics/LunyCollider.cs
@@ -2,14 +2,19 @@ using System;
 
 namespace Luny.Engine.Bridge.Physics
 {
-	public sealed class LunyCollider
+	public abstract class LunyCollider
 	{
-		public Object NativeObject { get; set; }
+		protected Object _nativeObject;
+		public Object NativeObject => _nativeObject;
 
-		public String Tag => throw new NotImplementedException();
-		public String Name => throw new NotImplementedException();
-		public String LayerName => throw new NotImplementedException();
-		public Int32 LayerIndex => throw new NotImplementedException();
-		public Boolean HasComponent(Type type) => throw new NotImplementedException();
+		public abstract String Tag { get; }
+		public abstract String Name { get; }
+		public abstract String LayerName { get; }
+		public abstract Int32 Layer { get; }
+
+		protected LunyCollider() {}
+		protected LunyCollider(Object collider) => _nativeObject = collider;
+
+		public abstract Boolean HasComponent(Type type);
 	}
 }
--- Engine/Bridge/Physics/LunyCollision.cs
+++ Engine/Bridge/Physics/LunyCollision.cs
@@ -4,12 +4,20 @@ namespace Luny.Engine.Bridge.Physics
 {
 	public abstract class LunyCollision
 	{
-		public Object NativeObject { get; set; }
+		protected Object _nativeObject;
 
+		public Object NativeObject => _nativeObject;
+
+		public abstract LunyCollider Collider { get; }
 		public abstract String Tag { get; }
 		public abstract String Name { get; }
 		public abstract String LayerName { get; }
 		public abstract Int32 Layer { get; }
+
+		protected LunyCollision() {}
+
+		protected LunyCollision(Object collision) => _nativeObject = collision;
+
 		public abstract Boolean HasComponent(Type type);
 	}
 }
