da7bd497e1a83f8f12858c174b984bcedce8dbd6
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-03-04 11:28:02 +0100

    fix missingref with collider

--- Engine/Bridge/Physics/LunyCollider.cs
+++ Engine/Bridge/Physics/LunyCollider.cs
@@ -4,16 +4,19 @@ namespace Luny.Engine.Bridge.Physics
 {
 	public abstract class LunyCollider
 	{
-		protected Object _nativeObject;
-		public Object NativeObject => _nativeObject;
+		public Object NativeCollider { get; private set; }
+		public Boolean IsValid { get; private set; }
 
 		public abstract String Tag { get; }
 		public abstract String Name { get; }
 		public abstract String LayerName { get; }
 		public abstract Int32 Layer { get; }
 
-		protected LunyCollider() {}
-		protected LunyCollider(Object collider) => _nativeObject = collider;
+		public void SetNativeCollider(Object nativeCollider)
+		{
+			IsValid = nativeCollider != null;
+			NativeCollider = nativeCollider;
+		}
 
 		public abstract Boolean HasComponent(Type type);
 	}
--- Engine/Bridge/Physics/LunyCollision.cs
+++ Engine/Bridge/Physics/LunyCollision.cs
@@ -4,19 +4,20 @@ namespace Luny.Engine.Bridge.Physics
 {
 	public abstract class LunyCollision
 	{
-		protected Object _nativeObject;
+		public Object NativeCollision { get; private set; }
+		public Boolean IsValid { get; private set; }
 
-		public Object NativeObject => _nativeObject;
-
-		public abstract LunyCollider Collider { get; }
 		public abstract String Tag { get; }
 		public abstract String Name { get; }
 		public abstract String LayerName { get; }
 		public abstract Int32 Layer { get; }
+		public abstract LunyCollider Collider { get; }
 
-		protected LunyCollision() {}
-
-		protected LunyCollision(Object collision) => _nativeObject = collision;
+		public virtual void SetNativeCollision(Object nativeCollision)
+		{
+			IsValid = nativeCollision != null;
+			NativeCollision = nativeCollision;
+		}
 
 		public abstract Boolean HasComponent(Type type);
 	}
